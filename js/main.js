ea68a65c1e1d672faf7e1528e12dc11734f04f90
marcus <marcus@kali.marcus>
2022-08-21 04:36:40 -0400

    here we go

---
+++ js/main.js
@@ -0,0 +1,17 @@
+    let fullName = prompt(`Please, enter your full name`);
+
+    let age = prompt(`Please, enter your age, ${fullName}`);
+
+    let gender = prompt("Please, enter your gender(male or female)");
+    
+    let phoneNumber = prompt(`Please, enter your phone number`);
+    
+    let email = prompt(`Please, enter your email, ${fullName}`);
+   
+    let personalInformation = prompt(`Please check your information:  ${fullName}  ${age}  ${gender}  ${phoneNumber}  ${email}, Is it true ? (yes or no)`);
+   
+    let goingObn = prompt(`${fullName}, if you want to buy, we carrying on (yes or no)`);
+
+    let  okay= prompt(`Okay, which is your want to buy our product: Iphone13, Iphone13Pro or Iphone13ProMax ?`);
+
+    let confirm = alert(`Thank you for leaving a request for an order, our responsible staff will contact you by phone, shortly`);
